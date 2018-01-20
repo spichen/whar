831874eb3e5c0bf277a161f0e8568e599cf90ac6
Salah <nh.salah@gmail.com>
2018-01-21 00:00:31 +0530

    Signout added

--- app/src/main/java/com/zigzag/whar/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/zigzag/whar/ui/dashboard/DashboardActivity.kt
@@ -3,13 +3,18 @@ package com.zigzag.whar.ui.dashboard
 import android.Manifest
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.Menu
 import com.tbruyelle.rxpermissions2.RxPermissions
 import com.zigzag.whar.R
 import com.zigzag.whar.arch.BaseActivity
 import com.zigzag.whar.common.ActivityUtils
 import com.zigzag.whar.ui.dashboard.feeds.FeedsFragment
 import com.zigzag.whar.ui.dashboard.fragments.PermissionFragment
+import kotlinx.android.synthetic.main.activity_dashboard.*
 import javax.inject.Inject
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+
 
 class DashboardActivity :  BaseActivity<DashboardContract.View, DashboardContract.Presenter>(), DashboardContract.View {
     @Inject
@@ -28,6 +33,9 @@ class DashboardActivity :  BaseActivity<DashboardContract.View, DashboardContrac
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        //toolbar.setLogo(R.drawable.logo)
 
         fragment = supportFragmentManager.findFragmentById(R.id.main_fragment_container)
 
@@ -54,4 +62,16 @@ class DashboardActivity :  BaseActivity<DashboardContract.View, DashboardContrac
         ActivityUtils.addFragmentToActivity(supportFragmentManager, fragment as PermissionFragment, R.id.main_fragment_container)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item?.itemId == R.id.item_logout){
+            FirebaseAuth.getInstance().signOut()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
